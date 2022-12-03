8f7bb2816d74205650daa3f8dcf7c3e48bad516c
ze__zeh <58066704+ze-zeh@users.noreply.github.com>
2022-12-04 01:16:26 +0900

    [Silver IV] Title: 30, Time: 388 ms, Memory: 32804 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.kt
+++ 백준/Silver/10610. 30/30.kt
@@ -1,10 +1,16 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
 fun main() {
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
     val n = readLine()!!.map { it }.sortedDescending()
     var sum = 0
     for (num in n) sum += num.digitToInt()
     if (!n.contains('0') || sum % 3 != 0) {
         print(-1)
     } else {
-        for(num in n) print(num)
+        for (num in n) bw.write("$num")
     }
+    bw.flush()
+    bw.close()
 }
