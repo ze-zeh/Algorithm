3567ed8c4e69268ed109b4154a6d0472d348f472
ze__zeh <58066704+ze-zeh@users.noreply.github.com>
2022-12-13 21:58:33 +0900

    [Silver III] Title: 통계학, Time: 276 ms, Memory: 40768 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.kt
+++ 백준/Silver/2108. 통계학/통계학.kt
@@ -1,25 +1,60 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.roundToInt
+
+fun getMedian(arr: IntArray, n: Int): Int {
+    var median = 0
+    var count = 0
+    for (i in 0..8000) {
+        count += arr[i]
+        if (count > n / 2) {
+            median = i - 4000
+            break
+        }
+    }
+    return median
+}
+
+fun getMode(arr: IntArray, maxCount: Int): Int {
+    var mode = 0
+    var count = 0
+
+    arr.forEachIndexed { index, it ->
+        if (maxCount == it) {
+            mode = index
+            count++
+            if (count == 2) {
+                return mode - 4000
+            }
+        }
+    }
+
+    return mode - 4000
+}
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
-    val numbers = mutableListOf<Int>()
+    val arr = IntArray(8001) { 0 }
+    var sum = 0
+    var max = -4000
+    var min = 4000
+    var maxCount = 0
+
     repeat(n) {
-        numbers.add(br.readLine().toInt())
+        val num = br.readLine().toInt()
+        sum += num
+        max = max(max, num)
+        min = min(min, num)
+        arr[num + 4000]++
+        maxCount = max(maxCount, arr[num + 4000])
     }
 
-    println("${numbers.average().roundToInt()}")
-    numbers.sort()
-    println("${numbers[n / 2]}")
-    val freq = numbers.groupingBy { it }.eachCount()
-    val max = freq.filter { it.value == freq.maxOf { it.value } }.keys.sorted()
-    val maxFreq = if(max.size == 1) {
-        max.first()
-    } else {
-        max[1]
-    }
-    println("$maxFreq")
-    println("${numbers.maxOf { it } - numbers.minOf { it }}")
+    val average = (sum.toDouble() / n).roundToInt()
+    println(average)
+    println(getMedian(arr, n))
+    println(getMode(arr, maxCount))
+    println(max - min)
 }
