e57d3e0c2e9d68a9ab240e86a6cda4a086144e8e
ze__zeh <58066704+ze-zeh@users.noreply.github.com>
2022-12-13 22:35:37 +0900

    [Silver V] Title: 영화감독 숌, Time: 124 ms, Memory: 12388 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.kt
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.kt
@@ -5,15 +5,19 @@ fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
     var count = 0
-    var num = 666
-    while(true) {
-        if(num.toString().contains("666")) {
-            count++
-            if(count == n) {
-                print(num)
+    var num = 665
+    var temp: Int
+
+    while (count != n) {
+        temp = ++num
+        while (temp >= 666) {
+            if (temp % 1000 == 666) {
+                count++
                 break
+            } else {
+                temp /= 10
             }
         }
-        num++
     }
+    print(num)
 }
