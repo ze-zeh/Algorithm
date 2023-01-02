ea47a1c7ba8d5db5c3182d647ec00e8ba699511e
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-01-02 17:25:16 +0900

    [Silver III] Title: 문자열 집합, Time: 4676 ms, Memory: 46628 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val s = arrayListOf<String>()
+    var count = 0
+
+    repeat(n) {
+        s.add(br.readLine())
+    }
+
+    repeat(m) {
+        if (s.contains(br.readLine())) count++
+    }
+
+    bw.write("$count")
+    bw.flush()
+    bw.close()
+    br.close()
+}
