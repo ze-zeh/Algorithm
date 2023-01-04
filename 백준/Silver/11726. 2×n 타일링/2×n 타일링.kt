8420314f360b169f6b7fd4f3771dcbd705b9d852
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-01-04 16:52:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 88 ms, Memory: 12276 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
@@ -3,20 +3,20 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
-fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val n = br.readLine().toInt()
-    val dp = Array(1001) { -1L }
-    dp[0] = 0L
-    dp[1] = 1L
-    dp[2] = 2L
+val br = BufferedReader(InputStreamReader(System.`in`))
+val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    for (i in 3..n) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007L
+fun main() {
+    val n = br.readLine().toInt()
+    val dp = IntArray(n + 1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for (i in 2..n) {
+        dp[i] = (dp[i - 1] % 10007 + dp[i - 2] % 10007) % 10007
     }
 
-    bw.write("${dp[n] % 10007L}\n")
+    bw.write("${dp[n]}")
     bw.flush()
     bw.close()
     br.close()
