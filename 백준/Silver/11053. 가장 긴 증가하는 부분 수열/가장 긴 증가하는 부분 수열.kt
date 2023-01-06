2b171d64e7f2b319f42a6212418b4f882b9bcd6e
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-01-06 16:30:20 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 148 ms, Memory: 21108 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.kt
@@ -2,30 +2,49 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.lang.Integer.max
 
 val br = BufferedReader(InputStreamReader(System.`in`))
 val bw = BufferedWriter(OutputStreamWriter(System.out))
 
 fun main() {
     val n = br.readLine().toInt()
-    val dp = Array(n + 1) { 0 }
+    val dp = Array(n) { 0 }
     val arr = mutableListOf<Int>()
-    arr.add(0)
+    var last = 0
     br.readLine().split(" ").map { it.toInt() }.forEach { arr.add(it) }
 
-    for (i in 1..n) {
-        var max = 0
-        for (j in 0 until i) {
-            if (arr[i] > arr[j]) {
-                max = max(dp[j], max)
-            }
+    for (i in arr.indices) {
+        if (i == 0) {
+            dp[last++] = arr[0]
+            continue
         }
-        dp[i] = max + 1
+
+        if (dp[last - 1] > arr[i]) {
+            dp[binarySearch(dp, 0, last - 1, arr[i])] = arr[i]
+        } else if (dp[last - 1] < arr[i]) {
+            dp[last++] = arr[i]
+        }
     }
 
-    bw.write("${dp.maxOrNull()}")
+    bw.write("$last")
     bw.flush()
     bw.close()
     br.close()
+}
+
+fun binarySearch(arr: Array<Int>, l: Int, h: Int, num: Int): Int {
+    var low = l
+    var high = h
+    var mid = 0
+
+    while (low < high) {
+        mid = (low + high) / 2
+
+        if (arr[mid] < num) {
+            low = mid + 1
+        } else {
+            high = mid
+        }
+    }
+    return high
 }
