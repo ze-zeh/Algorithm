1d37ed415d764625be3b5ccaddeff770aff44c60
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-01-16 22:51:19 +0900

    [Silver II] Title: 쇠막대기, Time: 132 ms, Memory: 14748 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        String str = br.readLine();
+        int count = 0;
+        boolean laser = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                stack.push('(');
+                laser = true;
+            } else {
+                if (laser) {
+                    count += stack.size() - 1;
+                } else {
+                    count++;
+                }
+                stack.pop();
+                laser = false;
+            }
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+}
