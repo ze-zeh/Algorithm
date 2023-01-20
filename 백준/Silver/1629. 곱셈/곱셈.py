0f24e8bdff2b934a6b742b545c42c97cc1d701a5
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-01-20 21:40:53 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,1 +1,14 @@
-print(pow(*map(int,input().split())))
+a, b, c = map(int, input().split())
+
+
+def multi(a, n):
+    if n == 1:
+        return a % c
+    else:
+        if n % 2 == 0:
+            return multi(a, n // 2) ** 2 % c
+        else:
+            return multi(a, n // 2) ** 2 * a % c
+
+
+print(multi(a, b))
