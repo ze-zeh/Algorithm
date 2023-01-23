ea10b67d7b7d8e260e32ef58538b00755e398f2e
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-01-23 22:29:26 +0900

    [Silver IV] Title: 큐 2, Time: 2092 ms, Memory: 354120 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        int num = -1;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if (st.countTokens() == 1) {
+                num = Integer.parseInt(st.nextToken());
+            }
+
+            switch (command) {
+                case "push":
+                    queue.add(num);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.poll() + "\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(queue.size() + "\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        bw.write("1\n");
+                    } else {
+                        bw.write("0\n");
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(queue.peek() + "\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(num + "\n");
+                    }
+                    break;
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
