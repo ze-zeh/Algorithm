5d527c911c20134e8724a4a3934587fd07ebbe32
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-01-30 14:49:43 +0900

    [Silver I] Title: Z, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -4,14 +4,7 @@ N, r, c = map(int, input().split())
 def z(n, row, col):
     if n == 0:
         return 0
-    half = 2 ** (n - 1)
-    if row < half and col < half:
-        return z(n - 1, row, col)
-    if row < half and col >= half:
-        return half ** 2 + z(n - 1, row, col - half)
-    if row >= half and col < half:
-        return 2 * half ** 2 + z(n - 1, row - half, col)
-    return 3 * half ** 2 + z(n - 1, row - half, col - half)
+    return 2 * (row % 2) + (col % 2) + 4 * z(n - 1, int(row / 2), int(col / 2))
 
 
 print(z(N, r, c))
