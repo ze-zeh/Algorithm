57a3a677b6b3b382efb93b43de97deeaa06f1320
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-02-02 07:41:40 +0900

    [Silver II] Title: 부분수열의 합, Time: 236 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -6,18 +6,15 @@ count = 0
 def solve(idx, sum):
     global count
 
-    if idx >= n:
+    if idx == n:
+        if sum == s:
+            count += 1
         return
-
     solve(idx + 1, sum)
-
-    sum += arr[idx]
-
-    if sum == s:
-        count += 1
-
-    solve(idx + 1, sum)
+    solve(idx + 1, sum + arr[idx])
 
 
 solve(0, 0)
+if s == 0:
+    count -= 1
 print(count)
