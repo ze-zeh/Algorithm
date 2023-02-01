dcc38ba3fe941debb6a4a49e8662383009470813
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-02-02 07:35:30 +0900

    [Silver II] Title: 부분수열의 합, Time: 244 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,14 +1,23 @@
-from itertools import combinations
-
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 count = 0
 
-for i in range(1, n + 1):
-    c = combinations(arr, i)
 
-    for x in c:
-        if sum(x) == s:
-            count += 1
+def solve(idx, sum):
+    global count
 
+    if idx >= n:
+        return
+
+    solve(idx + 1, sum)
+
+    sum += arr[idx]
+
+    if sum == s:
+        count += 1
+
+    solve(idx + 1, sum)
+
+
+solve(0, 0)
 print(count)
