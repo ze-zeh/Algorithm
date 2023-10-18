be320e4d9566a6a6fe4dbc44f59e070bff775ea1
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-10-19 01:50:09 +0900

    [Gold IV] Title: 부분합, Time: 228 ms, Memory: 22656 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N + 1];
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        while (end <= N) {
+            if (sum >= S) {
+                min = Math.min(min, end - start);
+                sum -= arr[start++];
+            } else {
+                sum += arr[end++];
+            }
+        }
+
+        if (min == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(min);
+        }
+    }
+}
