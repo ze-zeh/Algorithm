631872ff0f7e6a9939788067e0271a41a424251a
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-02-17 16:25:27 +0900

    [Gold V] Title: 동전 1, Time: 88 ms, Memory: 11772 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static public void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] dp = new int[k + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+
+            for (int j = arr[i]; j <= k; j++) {
+                dp[j] += dp[j - arr[i]];
+            }
+        }
+
+        System.out.print(dp[k]);
+
+        br.close();
+    }
+}
