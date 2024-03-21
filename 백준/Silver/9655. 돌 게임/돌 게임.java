13d6d7deb13a16abe4b052114c40568e8b2e6f3c
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-03-21 23:59:35 +0900

    [Silver V] Title: 돌 게임, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Main {
+    static public void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println((n % 2 == 1) ? "SK" : "CY");
+        br.close();
+    }
+}
