160cb062f50fa78dc21ba8e9e1e56c9d925c2825
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-10-30 18:45:22 +0900

    [level 2] Title: 큰 수 만들기, Time: 7005.93 ms, Memory: 94.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder sb = new StringBuilder();        
+        int start = 0;
+
+        for (int i = 0; i < number.length() - k; i++){
+            int max = 0;
+            
+            for (int j = start; j <= i + k; j++){
+                if (number.charAt(j) - '0' > max) {
+                    max = number.charAt(j) - '0';
+                    start = j + 1;
+                }
+            }
+            
+            sb.append(max);
+        }
+        
+        return sb.toString();
+    }
+}
