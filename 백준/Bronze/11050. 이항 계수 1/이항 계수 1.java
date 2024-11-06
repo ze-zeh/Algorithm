2c59273ba4656ba3e1f4dbefb1eaa3edb329a7ce
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-11-06 20:56:06 +0900

    [Bronze I] Title: 이항 계수 1, Time: 68 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] dp = new int[N + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 1] * i);
+        }
+
+        System.out.println((dp[N] / (dp[N - K] * dp[K])));
+
+        br.close();
+    }
+}
