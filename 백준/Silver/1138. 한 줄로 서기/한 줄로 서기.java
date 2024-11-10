c684b9be0c2fd586aa5ab3dbd5fcbf882dc8e4cb
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-11-10 17:21:30 +0900

    [Silver II] Title: 한 줄로 서기, Time: 64 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] answer = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (answer[j] != 0) continue;
+
+                if (arr[i] == 0) {
+                    answer[j] = i + 1;
+                    break;
+                } else {
+                    arr[i]--;
+                }
+            }
+        }
+
+        for (int a : answer) {
+            System.out.print(a + " ");
+        }
+    }
+}
