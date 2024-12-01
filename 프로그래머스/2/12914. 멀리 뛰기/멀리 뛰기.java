59d242fc58d726edad1ae45ef5e560cc6dc047d9
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-12-02 03:22:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        long[] dp = new long[n + 1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
