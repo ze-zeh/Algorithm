c91c5caffafe4a280a059afdfd20c9240d88d703
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-12-08 23:17:52 +0900

    [level 1] Title: 기사단원의 무기, Time: 98.36 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        for (int i = 1; i <= number; i++){
+            int count = 0;
+            
+            for (int j = 1; j * j <= i; j++){
+                if (j * j == i) {
+                    count++;
+                } else if(i % j == 0) {
+                    count += 2;
+                }
+            }
+            
+            if (count > limit) {
+                count = power;
+            }
+            
+            answer += count;
+        }
+        
+        return answer;
+    }
+}
