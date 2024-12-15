46cf76b3a857b9af1a24fcee6a5f118777cabac0
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2024-12-15 20:44:12 +0900

    [level 3] Title: 야근 지수, Time: 128.96 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,15 +3,17 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
         for (int work : works) {
             pq.add(work);
         }
         
-        while (n-- > 0 && pq.peek() != 0) {
+        while (n > 0) {
+            if (pq.peek() == 0) break;
+            
             pq.add(pq.poll() - 1);
+            n--;
         }
         
         for (int i = 0; i < works.length; i++) {
