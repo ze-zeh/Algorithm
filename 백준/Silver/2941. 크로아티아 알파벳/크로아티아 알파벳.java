2b4a010ab73126eb3b45a069d46d757a424e60f9
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2025-01-01 14:24:19 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 64 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -5,16 +5,12 @@ class Main {
     static public void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String A = br.readLine();
+        String[] arr = {"c=" , "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
-        A = A.replace("c=", "0");
-        A = A.replace("c-", "0");
-        A = A.replace("dz=", "0");
-        A = A.replace("d-", "0");
-        A = A.replace("lj", "0");
-        A = A.replace("nj", "0");
-        A = A.replace("s=", "0");
-        A = A.replace("z=", "0");
-
+        for (String s : arr) {
+            A = A.replace(s, "0");
+        }
+        
         System.out.println(A.length());
         br.close();
     }
