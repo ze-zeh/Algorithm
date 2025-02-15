a296e673df4687dc3038e260687480612fe36b70
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2025-02-15 21:56:45 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> list = new ArrayList<>();
+        long f = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+            f *= i;
+        }
+        
+        k--;
+        
+        int idx = 0;
+        
+        while (idx < n) {
+            f = f / (n - idx);
+            answer[idx++] = list.remove((int) (k / f));
+            k = k % f;
+        }
+        
+        return answer;
+    }
+}
