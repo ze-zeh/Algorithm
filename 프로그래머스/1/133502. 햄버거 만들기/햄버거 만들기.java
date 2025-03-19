39837e893c5611b64e317f0deacd3aa11af99c4a
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2025-03-19 22:10:53 +0900

    [level 1] Title: 햄버거 만들기, Time: 72.47 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -8,18 +8,18 @@ class Solution {
         for (int i : ingredient) {
             list.add(i);
             
-            if (list.size() >= 4) {
-                boolean first = list.get(list.size() - 4) == 1;
-                boolean second = list.get(list.size() - 3) == 2;
-                boolean third = list.get(list.size() - 2) == 3;
-                boolean fourth = list.get(list.size() - 1) == 1;
+            if (list.size() < 4) continue;
+            
+            boolean first = list.get(list.size() - 4) == 1;
+            boolean second = list.get(list.size() - 3) == 2;
+            boolean third = list.get(list.size() - 2) == 3;
+            boolean fourth = list.get(list.size() - 1) == 1;
                 
-                if (first && second && third && fourth) {
-                    answer++;
+            if (first && second && third && fourth) {
+                answer++;
                     
-                    for (int j = 0; j < 4; j++) {
-                        list.remove(list.size() - 1);
-                    }
+                for (int j = 0; j < 4; j++) {
+                    list.remove(list.size() - 1);
                 }
             }
         }
