d588676666f5a8829f738cf33cfc50862f80d1b9
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2025-03-24 23:21:10 +0900

    [level 1] Title: 덧칠하기, Time: 1.85 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,22 +1,15 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        boolean[] walls = new boolean[n];
-        
-        for (int s : section) {
-            walls[s - 1] = true;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            if (walls[i]) {
+        int current = 1;
+
+        for (int i = 0; i < section.length; i++) {
+            if (current <= section[i]) {
+                current = section[i] + m;
                 answer++;
-                
-                for (int j = 0; j < m && i + j < n; j++) {
-                    walls[i + j] = false;
-                }
             }
         }
-         
+
         return answer;
     }
 }
