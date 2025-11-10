9f2b83be96fc8602ef8078adc0380c03bb86f531
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2025-11-10 14:30:16 +0900

    [Gold IV] Title: 낚시, Time: 3424 ms, Memory: 316528 KB -BaekjoonHub

---
+++ 백준/Gold/30461. 낚시/낚시.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M, Q;
+    static int[][] Lake;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+        Lake = new int[N + 2][M + 2];
+
+        for (int i = 2; i <= N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 2; j <= M + 1; j++) {
+                Lake[i][j] = Lake[i - 1][j] + Lake[i - 1][j - 1] - Lake[i - 2][j - 1] + Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int w = Integer.parseInt(st.nextToken());
+            int q = Integer.parseInt(st.nextToken());
+
+            System.out.println(Lake[w + 1][q + 1]);
+        }
+
+        br.close();
+    }
+}
