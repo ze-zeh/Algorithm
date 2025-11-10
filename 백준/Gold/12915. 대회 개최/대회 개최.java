af87fe7f6924ebf0464adce580c870387df794bd
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2025-11-10 12:50:10 +0900

    [Gold V] Title: 대회 개최, Time: 72 ms, Memory: 11720 KB -BaekjoonHub

---
+++ 백준/Gold/12915. 대회 개최/대회 개최.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] d = new int[5];
+        int answer = 0;
+
+        for (int i = 0; i < 5; i++) {
+            d[i] = Integer.parseInt(st.nextToken());
+        }
+
+        while (true) {
+            boolean isPossible = true;
+
+            if (d[0] > 0) {
+                d[0]--;
+            } else {
+                if (d[1] > 0) {
+                    d[1]--;
+                } else {
+                    isPossible = false;
+                }
+            }
+
+            if (d[4] > 0) {
+                d[4]--;
+            } else {
+                if (d[3] > 0) {
+                    d[3]--;
+                } else {
+                    isPossible = false;
+                }
+            }
+
+            if (d[2] > 0) {
+                d[2]--;
+            } else {
+                if (d[0] + d[1] > d[3] + d[4]) {
+                    if (d[1] > 0) {
+                        d[1]--;
+                    } else {
+                        if (d[3] > 0) {
+                            d[3]--;
+                        } else {
+                            isPossible = false;
+                        }
+                    }
+                } else {
+                    if (d[3] > 0) {
+                        d[3]--;
+                    } else {
+                        if (d[1] > 0) {
+                            d[1]--;
+                        } else {
+                            isPossible = false;
+                        }
+                    }
+                }
+            }
+
+            if (!isPossible) {
+                break;
+            }
+
+            answer++;
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
