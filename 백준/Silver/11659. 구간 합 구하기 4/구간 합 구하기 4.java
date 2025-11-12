a44e771f1f048a3426b1c510ae931fc23887f213
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2025-11-12 09:56:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1604 ms, Memory: 70916 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,31 +2,28 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] dp = new int[N];
-        st = new StringTokenizer(br.readLine());
-        dp[0] = Integer.parseInt(st.nextToken());
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] prefix = new int[N + 1];
 
-        for (int i = 1; i < N; i++) {
-            dp[i] = dp[i - 1] + Integer.parseInt(st.nextToken());
-        }
+		st = new StringTokenizer(br.readLine());
 
-        for (int count = 0; count < M; count++) {
-            st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			prefix[i] = prefix[i - 1] + Integer.parseInt(st.nextToken());
+		}
 
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
+		for (int k = 0; k < M; k++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
 
-            if(i > 1) {
-                System.out.println(dp[j - 1] - dp[i - 2]);
-            } else {
-                System.out.println(dp[j - 1]);
-            }
-        }
-    }
+			System.out.println(prefix[j] - prefix[i - 1]);
+		}
+
+		br.close();
+	}
 }
