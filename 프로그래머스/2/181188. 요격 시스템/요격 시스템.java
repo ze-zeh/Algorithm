229ff4f39c86be7e3e2e3cfea02df770bc7710f3
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2026-06-22 18:34:50 +0900

    [level 2] Title: 요격 시스템, Time: 241.21 ms, Memory: 179 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -3,13 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        int end = 0;
+        int last = 0;
         
-        Arrays.sort(targets, (o1, o2) -> o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0]);
+        Arrays.sort(targets,(o1, o2) -> o1[1] == o2[1] ? o2[0] - o1[0] : o1[1] - o2[1]);
         
-        for (int[] target : targets){
-            if (end <= target[0]){
-                end = target[1];
+        for (int[] target : targets) {
+            if (last <= target[0]) {
+                last = target[1];
                 answer++;
             }
         }
